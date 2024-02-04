6c496e06b0e0c8bfb9da9fafc4dd0efd92256bbe
alzheimeer <alzheimeer@hotmail.com>
2024-02-03 20:49:43 -0500

    primer fase

---
+++ primero/app/(general)/pricing/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'SEO Title',
+ description: 'SEO Title',
+};
+
+
+export default function PricingPage() {
+  return (
+    <span className="text-7xl">Pricing Page</span>
+  );
+}
