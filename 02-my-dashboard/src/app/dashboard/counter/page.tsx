c30a61bfd0db91e7429519f00e807c66460c7826
alzheimeer <alzheimeer@hotmail.com>
2024-02-05 14:34:47 -0500

    ejercicio 2 complete

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,8 +1,19 @@
+import { CartCounter } from "@/app/shopping-cart";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'Shoping Cart Counter',
+ description: 'Un simple contador',
+};
 
 export default function CounterPage() {
+
+
+
   return (
-    <div>
-      <h1>Page Counter</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compras</span>
+      <CartCounter value={20}  /> 
     </div>
   );
 }
