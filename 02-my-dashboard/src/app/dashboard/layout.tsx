806485fec908ba13ae3ea7bc3047218314a3620a
alzheimeer <alzheimeer@hotmail.com>
2024-02-10 21:08:52 -0500

    seccion 5

--- 02-my-dashboard/src/app/dashboard/layout.tsx
+++ 02-my-dashboard/src/app/dashboard/layout.tsx
@@ -1,11 +1,11 @@
-import { Sidebar } from "../component";
+import { Sidebar } from "../../component";
 
 export default function DashboardLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
       <div className="flex">
        <Sidebar />
-        <div className="p-2 text-slate-900 w-full ">{children}</div>
+        <div className="text-slate-900 w-full ">{children}</div>
       </div>
     </div>
   );
--- 02-my-dashboard/src/component/Sidebar.tsx
+++ 02-my-dashboard/src/component/Sidebar.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import { IoBrowsersOutline, IoCalculator, IoGameController } from 'react-icons/io5';
+import { IoBrowsersOutline, IoCalculator, IoFootball, IoGameController } from 'react-icons/io5';
 import { SidebarMenuItem } from './SidebarMenuItem';
-import path from 'path';
+
 
 const menuItems = [
   {
@@ -15,6 +15,12 @@ const menuItems = [
     icon: <IoCalculator size={40} />,
     title: "Counter",
     subTitle: "Contador Client Side",
+  },
+  {
+    path: "/dashboard/pokemons",
+    icon: <IoFootball size={40} />,
+    title: "Pokemons",
+    subTitle: "Generacion Estatica",
   }
 ]
 
@@ -23,7 +29,7 @@ export const Sidebar = () => {
     <div
       id="menu"
       style={{ width: "400px" }}
-      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64  left-0 h-screen overflow-y-scroll"
+      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64  left-0 overflow-y-scroll"
     >
       <div id="logo" className="my-4 px-6">
         <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
